62249e6f86cd47a3a3781004b37887648369400a
Hugo <hugogarciahernandez281@gmail.com>
2026-03-06 12:22:28 +0100

    Clase 4 de marzo

--- 11 de febrero/Tema 2.3/Ejercicio22.cs
+++ 11 de febrero/Tema 2.3/Ejercicio22.cs
@@ -1,74 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-
-
 namespace Ejercicios_de_EM.Tema_2._3
 {
-    public class Ejercicio21
+    internal class Ejercicio22
     {
         static int ServerPort = 9050;
-
-        static void Client(object o)
+        static void Client()
         {
-            int minombre = (int)o;
             Socket client = new Socket(AddressFamily.InterNetwork,
                 SocketType.Dgram, ProtocolType.Udp);
-            EndPoint clientEP = new IPEndPoint(IPAddress.Loopback/*IPAddress.Parse("10.125.4.10")*/ /*Parse para conectarse a un servidor específico, en este caso el del profesor*/, ServerPort);
+            IPEndPoint clientEP = new IPEndPoint(IPAddress.Loopback/*IPAddress.Parse("10.125.4.10")*/ /*Parse para conectarse a un servidor específico, en este caso el del profesor*/, ServerPort);
 
-            byte[] msg = new byte[1024];
-            Thread.Sleep(100);
-            string msgWelcome = "Handshake initialized!";
 
-            //PASO 1: Cliente manda un saludo
-            msg = /*"Hello"*/Encoding.UTF8.GetBytes(msgWelcome);
-            
-            client.SendTo(msg, clientEP);
 
-            //PASO 4: Recibido el nuevo puerto, creo el nuevo socket
-            int recv = client.ReceiveFrom(msg, ref clientEP);
-            int nuevoPuerto = /*se puede poner solo como int*/ Int32.Parse(Encoding.UTF8.GetString(msg, 0, recv));
-
-            EndPoint nuevoConnectionServer = new IPEndPoint(IPAddress.Loopback, nuevoPuerto);
-            
-            //PASO 5: Mandar mensajes por el nuevo puerto
-            for (int i = 0; i < 10; i++)
+            while (true)
             {
-                msgWelcome = "Mensaje" + i;
-                Console.WriteLine("Cliente:" + minombre + msgWelcome);
-                msg = Encoding.UTF8.GetBytes(msgWelcome);
-                client.SendTo(msg, clientEP);
+                string msg = "Handshake initialized!";
+                msg = /*"Hello"*/Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(msg);
+                client.SendTo(data, clientEP);
 
-                if (msgWelcome == "EXIT") //De este modo cerramos el servidor abierto
+                if (msg == "EXIT") //De este modo cerramos el servidor abierto
                 {
                     break;
                 }
             }
 
-            try
-            {
-                client.Shutdown(SocketShutdown.Both);
-            }
-            finally
-            {
-                client.Close();
-            }
-
         }
 
-        static void ConnectionThread(object o)
+        static void ConnectionThread()
         {
-            //PASO 5: Escuchar al cliente por este hilo
-            int newServerPort = (int)o;
+            int newServerPort = (int)ServerPort;
             Socket connThread = new Socket(AddressFamily.InterNetwork,
                 SocketType.Dgram, ProtocolType.Udp);
 
@@ -89,6 +58,7 @@ namespace Ejercicios_de_EM.Tema_2._3
                     break;
                 }
             }
+
         }
 
         static void Server()
@@ -99,6 +69,7 @@ namespace Ejercicios_de_EM.Tema_2._3
             server.Bind(serverEP);
             Console.WriteLine("Server started at " + DateTime.UtcNow.ToString());
 
+            byte[] msg = new byte[1024];
             EndPoint remote = new IPEndPoint(IPAddress.Any, 0);
 
             for (int i = 0; i < 10; i++)
@@ -106,25 +77,14 @@ namespace Ejercicios_de_EM.Tema_2._3
                 //int recv = server.ReceiveFrom(msg, ref remote);
                 //string message = Encoding.UTF8.GetString(msg, 0, recv);
                 //Console.WriteLine(remote.ToString() + ":" + message);
-
-                byte[] msg = new byte[1024];
-
-                //PASO 2: Recibo el primer handshake
                 int recv = server.ReceiveFrom(msg, ref remote);
                 string message = Encoding.UTF8.GetString(msg, 0, recv);
                 Console.WriteLine(remote.ToString() + ":" + message);
-
-                //PASO 3: AVISAR AL CLIENTE del nuevo puerto
                 int nuevoPuerto = ServerPort + 1 + i;
-                msg = Encoding.UTF8.GetBytes(nuevoPuerto.ToString());
-                server.SendTo(msg, remote);
-
-                //PASO 4: Creo un nuevo hilo para comunicarme con el nuevo cliente
-                new Thread(ConnectionThread).Start(nuevoPuerto);
-
+                
                 //AVISAR AL CLIENTE NUEVO SERVER
 
-
+                new Thread(ConnectionThread).Start(nuevoPuerto);
 
                 //if (message == "EXIT") //De este modo se cierra el servidor abierto
                 //{
@@ -145,10 +105,7 @@ namespace Ejercicios_de_EM.Tema_2._3
 
         public static void Main(String[] args)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                new Thread(Client).Start();
-            }
+            new Thread(Client).Start();
             new Thread(Server).Start();
         }
     }
