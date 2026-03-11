b251db8c50410a0eff27253949f04c0e9fbb09bc
Hugo <hugogarciahernandez281@gmail.com>
2026-03-11 14:38:51 +0100

    Clase 11 de marzo

---
+++ 11 de febrero/Tema 2.3/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_de_EM.Tema_2._3
+{
+     class Class1
+    {
+        public delegate void Print(int value);
+        public static void PrintNumber(int num)
+        {
+            Console.WriteLine("Number: {0}", num);
+        }
+        public static void PrintMoney(int money)
+        {
+            Console.WriteLine("Money: {0}", money);
+        }
+        public static void Main()
+        {
+            Print printDelegate = PrintNumber;
+            printDelegate(1000);
+            printDelegate = PrintMoney;
+            printDelegate(1000);
+        }
+    }
+}
---
+++ 11 de febrero/Tema 4.1/Ejercicio1_Tema4_1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace _11_de_febrero
+{
+    public class Ejercicio1_Tema4_1
+    {
+        static Random random = new Random();
+        static volatile bool producido = false;
+        static volatile int producto = 0;
+
+         void Productor()
+        {
+            producto = random.Next(10);
+            //Genera un núumero aleatorio
+            Console.WriteLine("Productor: " + producto);
+            producido = true;
+        }
+         void Consumidor()
+        {
+            //Muestra el numero aleatorio
+            while (!producido)
+            {
+                Console.WriteLine("Esperando");
+            }
+            ;
+            Console.WriteLine("*******Consumidor*******: " + producto);
+        }
+
+        public void Exec()
+        {
+            //Declaración de un hilo con Action y con Invoke
+
+            Action met_productor = Productor;
+            //ThreadStart hilo = Productor;
+            ThreadStart hilo = met_productor.Invoke;
+            Thread prod = new Thread(hilo);
+            
+            //Thread prod = new Thread(() => Productor()); //Podemos usar una expresion lambda para indicar la funcion a la que queremos llamar, todo porque ThreadStart es un Delegate 
+            prod.Start();
+
+            Thread cons = new Thread(() => Consumidor());
+            cons.Start();
+        }
+
+        public static void Main(String[] args)
+        {
+            new Ejercicio1_Tema4_1().Exec();
+        }
+    }
+}
