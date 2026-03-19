f8099bc9c4b96172146ed1bcd888f2b9cda72a9f
Hugo <hugogarciahernandez281@gmail.com>
2026-03-19 01:27:09 +0100

    Exámenes de Móstoles y Quintana

---
+++ 11 de febrero/Examenes/ExamenParcialMostoles.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace _11_de_febrero
+{
+    internal class Mostoles
+    {
+        const int N_COCHES = 10;
+        const int M = 3;
+        const int vueltas = 12;
+        static volatile int terminado = 0;
+        static volatile int total = N_COCHES;
+        static volatile List<string> clasificacion = new List<string>();
+        static Random random = new Random();
+        static Mutex mutex = new Mutex();
+        static SemaphoreSlim barrera = new SemaphoreSlim(0);
+        static SemaphoreSlim correr = new SemaphoreSlim(0);
+        static SemaphoreSlim boxes = new SemaphoreSlim(M);
+        static SemaphoreSlim finalizar = new SemaphoreSlim(0);
+        static volatile int contadorCoches = 0;
+
+        public static void Coche(object id)
+        {
+            //Creación de variables locales necesarias para el desarrollo del apartado B
+            int intentos = 0;
+            bool intentando = false;
+
+            //Indicamos una zona de exclusión mutua para que solo cuando todos los coches estén, el semáforo se ponga verde
+            mutex.WaitOne();
+            ++contadorCoches;
+
+            if (contadorCoches == N_COCHES)
+            {
+                barrera.Release();
+            }
+            mutex.ReleaseMutex();
+
+            correr.Wait();
+
+            //Hacemos que el coche de 12 vueltas
+            for (int i = 0; i < vueltas; i++)
+            {
+                //Tiempo que tarda en dar una vuelta
+                Thread.Sleep(random.Next(700, 1000));
+
+                if (i % 4 == 0 || intentando) //Debido a la forma de diseñar el bucle for es innecesario indicar que no se tenga en cuenta la vuelta 12
+                {
+                    //Para comprobar si puede acceder a los boxes, el coche comprobará antes el número de permisos disponibles en el semáforo boxes
+                    if (boxes.CurrentCount > 0)
+                    {
+                        intentos = 0;
+                        boxes.Wait();
+                        Thread.Sleep(200);
+                        boxes.Release();
+                    }
+                    else //Si no hay sitio el coche se marcará a sí mismo una variable booleana a true para que vuelva a intentar entrar en la siguiente vuelta
+                    {
+                        intentando = true;
+                        WriteLine("Boxes ocupados", id);
+                        ++intentos;
+                        if (intentos == 2) //Si el coche ha intentado entrar dos veces sin exito a boxes, abandonará la carrera
+                        {
+                            intentando = false;
+                            WriteLine("Abandona", id);
+                            finalizar.Release();
+                            return;
+                        }
+                    }
+                }
+            }
+            mutex.WaitOne(); //Indicamos una nueva zona de exclusión mutua para indicar que 
+            clasificacion.Add("Coche " + id);
+            mutex.ReleaseMutex();
+
+            finalizar.Release();
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < N_COCHES; i++)
+            {
+                new Thread(Coche).Start(i);
+            }
+
+            barrera.Wait();
+
+            if (contadorCoches == N_COCHES)
+            {
+                Console.WriteLine("¡SEMÁFORO EN VERDE!");
+            }
+
+            correr.Release(N_COCHES);
+
+            for (int i = 0; i < N_COCHES; i++)
+            {
+                finalizar.Wait();
+            }
+
+            Console.WriteLine("Clasificacion final: ");
+            foreach (string i in clasificacion) { Console.WriteLine(i); }
+            Console.ReadLine();
+        }
+
+        static void WriteLine(string s, object id)
+        {
+            Console.WriteLine("Coche " + id + ": " + s);
+        }
+    }
+}
---
+++ 11 de febrero/Examenes/ExamenParcialQuintana.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace _11_de_febrero
+{
+    class Utils
+    {
+        public static Random Rand = new Random();
+
+        public static void SleepRandom(int millis)
+
+        {
+
+            Thread.Sleep(Rand.Next(0, millis));
+
+        }
+
+    }
+    class Dune
+    {
+        public static int NUM_GRANJAS = 10;
+
+        public static int NUM_MARTILLO = 5;
+
+        public static int ESPECIA_MAX = 8;
+
+
+
+        public static int NUM_EXPLOS = 3;
+
+        public static int COSTE_EXPLOS = 5;
+
+        //PARTE 1
+
+        static SemaphoreSlim martillosDisponibles = new SemaphoreSlim(NUM_MARTILLO); //Definimos un semaforo para seguir la restricción del número de martillos disponibles
+        static SemaphoreSlim esperarRecolectores = new SemaphoreSlim(0); //Definimos un semáforo para seguir la restricción que obliga al programa a esperar a todos los recolectores antes de iniciar un nuevo ciclo
+
+        static Mutex almacenMutex = new Mutex(); //Definimos un mutex para evitar sobrescribir/leer incorrectamente la variable global ALMACEN
+
+        static volatile int ALMACEN = 0;
+
+        //PARTE 2
+        static SemaphoreSlim huecosExploradores = new SemaphoreSlim(NUM_EXPLOS); //Definimos un semáforo para seguir la restricción que obliga al instructor a no producir más exploradores de los debidos
+
+        //PARTE 1
+        public static void Recolector(object id)
+        {
+            WriteLine(id, "Viajo a la granja de especia");
+
+            //Aplicamos la restricción del semáforo martillosDisponibles
+            martillosDisponibles.Wait();
+            int especia = Utils.Rand.Next(1, ESPECIA_MAX);
+            WriteLine("\t" + id, "He tomado " + especia + " especias");
+            martillosDisponibles.Release();
+
+            WriteLine(id, "Vuelvo a la base central");
+
+            //Evitamos hacer malas lecturas/escrituras en la variable ALMACEN
+            almacenMutex.WaitOne();
+            ALMACEN += especia;
+            almacenMutex.ReleaseMutex();
+
+            //Indicamos que el recolector ha acabado a través del semáforo de fin de ciclo: esperarRecolectores
+            esperarRecolectores.Release();
+        }
+
+        //PARTE 2
+        public static void Explorador(object id)
+        {
+            //El explorador explorar en un tiempo aleatorio y da su informe
+            Console.WriteLine("\t \tExplorador {0}: Explorando...", id);
+            Utils.SleepRandom(200);
+            Console.WriteLine("\t \tExplorador {0}: Mi informe", id);
+
+            //Al terminar, deja espacio a nuevos exploradores
+            huecosExploradores.Release();
+        }
+
+        public static void Instructor()
+        {
+            //Definimos un índice para identificar a nuevos exploradores
+            int i = 0;
+            //Mantenemos ejecutando a Instructor de forma infinita ya que requerimos que este trabaje continuamente
+            while (true)
+            {
+                almacenMutex.WaitOne();
+                if (ALMACEN >= COSTE_EXPLOS)
+                {
+                    //Restringimos la lectura/escritura de la variable global ALMACEN para evitar errores
+                    ALMACEN -= COSTE_EXPLOS;
+                    almacenMutex.ReleaseMutex();
+                    huecosExploradores.Wait();
+
+                    //Aumentamos el índice de exploradores
+                    i++;
+
+                    //Creamos un nuevo explorador cada vez que sea posible
+                    Thread explorador = new Thread(Explorador);
+                    explorador.Start(i);
+                }
+                else
+                {
+                    Console.WriteLine("\t\t\tInstructor: NO PUEDO CREAR EXPLORADORES, NO HAY SUFICIENTES ESPECIAS");
+                    almacenMutex.ReleaseMutex();
+                    //Thread.Sleep(100);
+
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //PARTE 2
+
+            //Creamos un único hilo Instructor que será quien controle la creación de nuevos exploradores
+            Thread instructor = new Thread(Instructor);
+            instructor.Start();
+
+            //PARTE 1
+            while (true) //Ejecutamos indefinidamente un número infinito de ciclos
+            {
+                //Inicializamos y arrancamos los hilos del nuevo ciclo
+                Console.WriteLine("NUEVO CICLO");
+                for (int i = 0; i < NUM_GRANJAS; i++)
+                {
+                    Thread granja = new Thread(Recolector);
+                    granja.Start(i);
+                }
+
+                //Detenemos la ejecución de Main hasta que todos los recolectores hayan acabado
+                for (int i = 0; i < NUM_GRANJAS; i++)
+                {
+                    esperarRecolectores.Wait();
+                }
+
+                //Se procesan e indican las especias del almacen, y se espera un tiempo aleatorio entre un ciclo y el siguiente
+                Console.WriteLine("EL ALMACEN CONTIENE:");
+
+                Console.WriteLine(ALMACEN + " especias\n");
+
+                Utils.SleepRandom(200);
+            }
+        }
+
+        //Creación de un método auxiliar que permita identificar a cada hilo
+        public static void WriteLine(object id, string s)
+        {
+            Console.WriteLine("Thread " + id + ": " + s);
+        }
+    }
+}
